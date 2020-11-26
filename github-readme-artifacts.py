7ba049890ef5e95ed5b6f7fa183d10de76cd67e9
nicholas <znicholasbrown@gmail.com>
2020-11-25 19:04:11 -0500

    Replace flow-level-artifact with github readme flow

---
+++ github-readme-artifacts.py
@@ -0,0 +1,37 @@
+import prefect
+from prefect import Flow, Task, artifacts, Parameter
+from prefect.client import Secret
+from prefect.environments.storage import GitHub
+from github import Github as GithubClient
+
+
+class GetReadMe(Task):
+    def run(self, ref):
+        access_token = Secret("GITHUB_AUTH_TOKEN").get()
+        github_client = GithubClient(access_token)
+
+        repo = github_client.get_repo(ref)
+        readme = repo.get_contents("README.md")
+
+        return readme.decoded_content
+
+
+class GenerateArtifact(Task):
+    def run(self, readme):
+        return artifacts.create_markdown(readme)
+
+
+with Flow("GitHub README Artifacts") as flow:
+    repo = Parameter("repo", "PrefectHQ/prefect")
+
+    readme = GetReadMe()(ref=repo)
+    GenerateArtifact()(readme=readme)
+
+
+flow.storage = GitHub(
+    repo="znicholasbrown/project-artifacts",
+    path="github-readme-artifacts.py",
+    secrets=["GITHUB_AUTH_TOKEN"],
+)
+
+flow.register(project_name="Artifacts")
